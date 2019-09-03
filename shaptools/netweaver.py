c5484fcf7e3861bdbf9aeec0eb540c9441b5724f
Xabier Arbulu Insausti <xarbulu@suse.com>
2019-09-03 12:15:32 +0200

    Add code to install ERS instance
    * sapcontrol command usage has been modified to support more parameters,
    specially to run the command in a different SAP NW host

--- shaptools/netweaver.py
+++ shaptools/netweaver.py
@@ -9,6 +9,7 @@ SAP Netweaver management module
 """
 
 import logging
+import time
 
 from shaptools import shell
 
@@ -33,14 +34,17 @@ class NetweaverInstance(object):
         sid (str): SAP Netweaver sid
         inst (str): SAP Netweaver instance number
         password (str): Netweaver instance password
+        remote_host (str, opt): Remote host where the command will be executed
     """
 
     # SID is usualy written uppercased, but the OS user is always created lower case.
     NETWEAVER_USER = '{sid}adm'.lower()
     UNINSTALL_PRODUCT = 'NW_Uninstall:GENERIC.IND.PD'
     GETPROCESSLIST_SUCCESS_CODES = [0, 3, 4]
+    SUCCESSFULLY_INSTALLED = 0
+    UNSPECIFIED_ERROR = 111
 
-    def __init__(self, sid, inst, password):
+    def __init__(self, sid, inst, password, **kwargs):
         # Force instance nr always with 2 positions.
         inst = '{:0>2}'.format(inst)
         if not all(isinstance(i, basestring) for i in [sid, inst, password]):
@@ -51,28 +55,56 @@ class NetweaverInstance(object):
         self.sid = sid
         self.inst = inst
         self._password = password
+        self.remote_host = kwargs.get('remote_host', None)
 
-    def _execute_sapcontrol(self, sapcontrol_function, exception=True):
+    def _execute_sapcontrol(self, sapcontrol_function, **kwargs):
         """
         Execute sapcontrol commands and return result
 
         Args:
             sapcontrol_function (str): sapcontrol function
             exception (boolean): Raise NetweaverError non-zero return code (default true)
+            host (str, optional): Host where the command will be executed
+            inst (str, optional): Use a different instance number
+            user (str, optional): Define a different user for the command
+            password (str, optional): The new user password
 
         Returns:
             ProcessResult: ProcessResult instance storing subprocess returncode,
                 stdout and stderr
         """
+        exception = kwargs.get('exception', True)
+        # The -host and -user parameters are used in sapcontrol to authorize commands execution
+        # in remote host Netweaver instances
+        host = kwargs.get('host', None)
+        inst = kwargs.get('inst', self.inst)
+        user = kwargs.get('user', None)
+        password = kwargs.get('password', None)
+        if user and not password:
+            raise NetweaverError('Password must be provided together with user')
+
+        host_str = '-host {} '.format(host) if host else ''
+        user_str = '-user {} {} '.format(user, password) if user else ''
+
         user = self.NETWEAVER_USER.format(sid=self.sid)
-        cmd = 'sapcontrol -nr {instance} -function {sapcontrol_function}'.format(
-            instance=self.inst, sapcontrol_function=sapcontrol_function)
-        result = shell.execute_cmd(cmd, user, self._password)
+        cmd = 'sapcontrol {host}{user}-nr {instance} -function {sapcontrol_function}'.format(
+            host=host_str, user=user_str, instance=inst, sapcontrol_function=sapcontrol_function)
+
+        result = shell.execute_cmd(cmd, user, self._password, self.remote_host)
 
         if exception and result.returncode != 0:
             raise NetweaverError('Error running sapcontrol command: {}'.format(result.cmd))
 
         return result
+
+    @staticmethod
+    def get_attribute_from_file(conf_file, attribute_pattern):
+        """
+        Get attribute from file using a pattern
+        """
+        with open(conf_file, 'r') as file_content:
+            attribute_data = shell.find_pattern(attribute_pattern, file_content.read())
+        return attribute_data
 
     @staticmethod
     def _is_ascs_installed(processes):
@@ -116,16 +148,24 @@ class NetweaverInstance(object):
             raise ValueError('provided sap instance type is not valid: {}'.format(sap_instance))
 
     @classmethod
-    def install(cls, software_path, virtual_host, product_id, conf_file, root_user, password):
+    def install(
+            cls, software_path, virtual_host, product_id, conf_file, root_user, password, **kwargs):
         """
         Install SAP Netweaver instance
 
         Args:
             software_path (str): Path where SAP Netweaver 'sapinst' tool is located
+            virtual_host (str): Virtual host name of the machine
+            product_id (str): SAP instance product id
             conf_file (str): Path to the configuration file
             root_user (str): Root user name
             password (str): Root user password
+            exception (bool, opt): Raise and exception in case of error if True, return result
+                object otherwise
+            remote_host (str, opt): Remote host where the command will be executed
         """
+        raise_exception = kwargs.get('exception', True)
+        remote_host = kwargs.get('remote_host', None)
 
         cmd = '{software_path}/sapinst SAPINST_USE_HOSTNAME={virtual_host} '\
             'SAPINST_EXECUTE_PRODUCT_ID={product_id} '\
@@ -135,31 +175,174 @@ class NetweaverInstance(object):
                 virtual_host=virtual_host,
                 product_id=product_id,
                 conf_file=conf_file)
-        result = shell.execute_cmd(cmd, root_user, password)
-        if result.returncode:
+        result = shell.execute_cmd(cmd, root_user, password, remote_host)
+        if result.returncode and raise_exception:
             raise NetweaverError('SAP Netweaver installation failed')
+        return result
 
-    def uninstall(self, software_path, virtual_host, conf_file, root_user, password):
+    @classmethod
+    def _ascs_restart_needed(cls, installation_result):
+        """
+        Check the ERS installation return code and output to see if the ASCS instance restart
+        is needed
+
+        Args:
+            installation_result (shell.ProcessResult): ERS installation result
+
+        Returns: True if ASCS restart is needed, False otherwise
+        """
+        expected_msg = \
+            '<html><p>Error when stopping instance.</p><p>Cannot stop instance <i>(.*)'\
+            '</i> on host <i>(.*)</i>.</p><p>Stop the instance manually and choose <i>OK</i> to '\
+            'continue.</html>'
+        if installation_result.returncode == cls.UNSPECIFIED_ERROR:
+            if shell.find_pattern(expected_msg, installation_result.output):
+                return True
+        return False
+
+    @classmethod
+    def _restart_ascs(cls, conf_file, ers_pass, ascs_pass, remote_host=None):
+        """
+        Restart ascs from the ERS host.
+
+        Args:
+            conf_file (str): Path to the configuration file
+            ascs_pass (str): ASCS instance password
+            remote_host (str, optional): Remote host where the command will be executed
+        """
+        # Get sid and instance number from configuration file
+        sid = cls.get_attribute_from_file(
+            conf_file, 'NW_readProfileDir.profileDir += +.*/(.*)/profile').group(1).lower()
+        instance_number = cls.get_attribute_from_file(
+            conf_file, 'nw_instance_ers.ersInstanceNumber += +(.*)').group(1)
+        ers = cls(sid, instance_number, ers_pass, remote_host=remote_host)
+        result = ers.get_system_instances(exception=False)
+        ascs_data = shell.find_pattern(
+            '(.*), (.*), (.*), (.*), (.*), MESSAGESERVER|ENQUE, GREEN', result.output)
+
+        ascs_user = '{}adm'.format(sid).lower()
+        ascs_hostname = ascs_data.group(1)
+        ascs_instance_number = ascs_data.group(2)
+
+        ers.stop(host=ascs_hostname, inst=ascs_instance_number, user=ascs_user, password=ascs_pass)
+        ers.start(host=ascs_hostname, inst=ascs_instance_number, user=ascs_user, password=ascs_pass)
+
+    @classmethod
+    def install_ers(
+            cls, software_path, virtual_host, product_id, conf_file, root_user, password, **kwargs):
+        """
+        Install SAP Netweaver ERS instance. ERS instance installation needs an active polling
+        the instance where the ASCS is installed.
+
+        Args:
+            software_path (str): Path where SAP Netweaver 'sapinst' tool is located
+            virtual_host (str): Virtual host name of the machine
+            product_id (str): SAP instance product id
+            conf_file (str): Path to the configuration file
+            root_user (str): Root user name
+            password (str): Root user password
+            ascs_password (str, optional): Password of the SAP user in the machine hosting the
+                ASCS instance. If it's not set the same password used to install ERS will be used
+            timeout (int, optional): Timeout of the installation process. If 0 it will try to
+                install the instance only once
+            interval (int, optional): Retry interval in seconds
+            remote_host (str, opt): Remote host where the command will be executed
+        """
+        timeout = kwargs.get('timeout', 0)
+        interval = kwargs.get('interval', 5)
+        ers_pass = cls.get_attribute_from_file(
+            conf_file, 'nwUsers.sidadmPassword += +(.*)').group(1)
+        ascs_pass = kwargs.get('ascs_password', ers_pass)
+        remote_host = kwargs.get('remote_host', None)
+
+        current_time = time.clock()
+        current_timeout = current_time + timeout
+        while current_time <= current_timeout:
+            result = NetweaverInstance.install(
+                software_path, virtual_host, product_id, conf_file, root_user, password,
+                exception=False, remote_host=remote_host)
+
+            if result.returncode == cls.SUCCESSFULLY_INSTALLED or \
+                    cls._ascs_restart_needed(result):
+                cls._restart_ascs(conf_file, ers_pass, ascs_pass, remote_host)
+                break
+
+            time.sleep(interval)
+            current_time = time.clock()
+        else:
+            raise NetweaverError(
+                'SAP Netweaver ERS installation failed after {} seconds'.format(timeout))
+
+    def uninstall(self, software_path, virtual_host, conf_file, root_user, password, **kwargs):
         """
         Uninstall SAP Netweaver instance
 
         Args:
             software_path (str): Path where SAP Netweaver 'sapinst' tool is located
+            virtual_host (str): Virtual host name of the machine
             conf_file (str): Path to the configuration file
             root_user (str): Root user name
             password (str): Root user password
+            remote_host (str, opt): Remote host where the command will be executed
         """
+        remote_host = kwargs.get('remote_host', None)
         user = self.NETWEAVER_USER.format(sid=self.sid)
         self.install(
-            software_path, virtual_host, self.UNINSTALL_PRODUCT, conf_file, root_user, password)
+            software_path, virtual_host, self.UNINSTALL_PRODUCT, conf_file, root_user, password,
+            remote_host=remote_host)
         shell.remove_user(user, True, root_user, password)
 
-    def get_process_list(self, exception=True):
+    def get_process_list(self, exception=True, **kwargs):
         """
         Get SAP processes list
         """
-        result = self._execute_sapcontrol('GetProcessList', exception=False)
+        result = self._execute_sapcontrol('GetProcessList', exception=False, **kwargs)
         if exception and result.returncode not in self.GETPROCESSLIST_SUCCESS_CODES:
             raise NetweaverError('Error running sapcontrol command: {}'.format(result.cmd))
-
         return result
+
+    def get_system_instances(self, exception=True, **kwargs):
+        """
+        Get SAP system instances list
+        """
+        result = self._execute_sapcontrol('GetSystemInstanceList', exception=False, **kwargs)
+        if exception and result.returncode:
+            raise NetweaverError('Error running sapcontrol command: {}'.format(result.cmd))
+        return result
+
+    def get_instance_properties(self, exception=True, **kwargs):
+        """
+        Get SAP instance properties
+        """
+        result = self._execute_sapcontrol('GetInstanceProperties', exception=False, **kwargs)
+        if exception and result.returncode:
+            raise NetweaverError('Error running sapcontrol command: {}'.format(result.cmd))
+        return result
+
+    def start(self, wait=15, delay=0, **kwargs):
+        """
+        Start SAP instance
+        Args:
+            wait (int): Time to wait until the processes are started in seconds
+        """
+        if wait:
+            cmd = 'StartWait {} {}'.format(wait, delay)
+        else:
+            cmd = 'Start'
+        result = self._execute_sapcontrol(cmd, exception=False, **kwargs)
+        if result.returncode:
+            raise NetweaverError('Error running sapcontrol command: {}'.format(result.cmd))
+
+    def stop(self, wait=15, delay=0, **kwargs):
+        """
+        Stop SAP instance
+        Args:
+            wait (int): Time to wait until the processes are stopped in seconds
+        """
+        if wait:
+            cmd = 'StopWait {} {}'.format(wait, delay)
+        else:
+            cmd = 'Stop'
+        result = self._execute_sapcontrol(cmd, exception=False, **kwargs)
+        if result.returncode:
+            raise NetweaverError('Error running sapcontrol command: {}'.format(result.cmd))

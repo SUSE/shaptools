3b7ada35e0561739ef3faf8e6710ed85de832cd1
Simranpal <simranpal.singh@suse.com>
2019-10-01 17:29:57 -0700

    Add UT for update_conf_file

--- tests/netweaver_test.py
+++ tests/netweaver_test.py
@@ -244,6 +244,22 @@ class TestNetweaver(unittest.TestCase):
         self._netweaver.get_process_list.assert_called_once_with(False)
         self._netweaver._is_ers_installed.assert_called_once_with(processes_mock)
 
+    def test_update_conf_file(self):
+
+        pwd = os.path.dirname(os.path.abspath(__file__))
+        shutil.copyfile(pwd+'/support/original.inifile.params', '/tmp/copy.inifile.params')
+        conf_file = netweaver.NetweaverInstance.update_conf_file(
+            '/tmp/copy.inifile.params', sid='HA1',
+            sidadmPassword='testpwd', masterPwd='Suse1234')
+        self.assertTrue(filecmp.cmp(pwd+'/support/modified.inifile.params', conf_file))
+
+        shutil.copyfile(pwd+'/support/original.inifile.params', '/tmp/copy.inifile.params')
+        conf_file = netweaver.NetweaverInstance.update_conf_file(
+            '/tmp/copy.inifile.params',
+            **{'NW_GetSidNoProfiles.sid': 'HA1', 'nwUsers.sidadmPassword': 'testpwd',
+               'NW_GetMasterPassword.masterPwd': 'Suse1234'})
+        self.assertTrue(filecmp.cmp(pwd+'/support/modified.inifile.params', conf_file))
+
     @mock.patch('shaptools.shell.execute_cmd')
     def test_install(self, mock_execute_cmd):
 

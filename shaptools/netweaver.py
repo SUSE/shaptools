ee7b8307c73d5b1aaf63041ffa82ecea58ded711
dmaiocchi <dmaiocchi@suse.com>
2020-09-17 22:09:38 +0200

    split lines

--- shaptools/netweaver.py
+++ shaptools/netweaver.py
@@ -244,11 +244,15 @@ class NetweaverInstance(object):
         result = shell.execute_cmd(cmd, root_user, password, remote_host)
         if result.returncode and raise_exception:
             if cwd:
-                raise NetweaverError('SAP Netweaver installation failed. Please check swpm installation logs' \
-                                     '(sapinst_dev.log and sapinst.log) located at {0} for further information'.format(cwd))
+                raise NetweaverError('SAP Netweaver installation failed.'\ 
+                                     ' Please check swpm installation logs'\
+                                     '(sapinst_dev.log and sapinst.log) located at {0}'\
+                                     ' for further information'.format(cwd))
             else:
-                raise NetweaverError('SAP Netweaver installation failed. Please check swpm installation logs' \
-                                     '(sapinst_dev.log and sapinst.log) located at /tmp/sapinst_instdir default folder for further information')
+                raise NetweaverError('SAP Netweaver installation failed.'\
+                                     ' Please check swpm installation logs'\
+                                     '(sapinst_dev.log and sapinst.log) located at'\
+                                     ' /tmp/sapinst_instdir default folder for further information')
         return result
 
     @classmethod
